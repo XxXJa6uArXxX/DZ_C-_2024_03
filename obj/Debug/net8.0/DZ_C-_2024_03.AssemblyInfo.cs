3001ed3dec21f3244cc80ad28554d86561018ba3
XxXJa6uArXxX <valera5xxxjaguarxxx5@mail.ru>
2024-03-13 01:47:47 +0500

    изменили файл

--- obj/Debug/net8.0/DZ_C-_2024_03.AssemblyInfo.cs
+++ obj/Debug/net8.0/DZ_C-_2024_03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DZ_C-_2024_03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c637f5e2aebd43922ed8ecc4a2dda133110f0a8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+271ae2d8c9944ccf14a3b622965b56c39bf878f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DZ_C-_2024_03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DZ_C-_2024_03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
